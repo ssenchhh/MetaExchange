0cf9683c485502d1d9aa67b52b6628270a765ea8
Oleksandr Slinkin <Slinkin.operative@gmail.com>
2023-08-24 13:09:02 +0300

    Created models

---
+++ MetaExchangeConsoleApp/Models/OrderWrapper.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MetaExchangeConsoleApp.Models
+{
+    public class OrderWrapper
+    {
+        [JsonPropertyName("Order")]
+        public Order Order { get; set; }
+    }
+}
